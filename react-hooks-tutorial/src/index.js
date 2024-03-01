c31b5d6ad4b31cff939063520aab76fbdcdb4235
Barry <ezeanibarry1@gmail.com>
2024-03-01 19:14:52 +0100

    Create multiple state Hooks

--- react-hooks-tutorial/src/index.js
+++ react-hooks-tutorial/src/index.js
@@ -1,30 +1,27 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 
-const FavoriteColor = () => {
+const Car = () => {
+  // const [car, setCar] = useState({
+  //   brand: "Ford",
+  //   model: "Mustang",
+  //   year: "1964",
+  //   color: "red"
+  // });
+  const [brand, setBrand] = useState("Ford");
+  const [model, setModel] = useState("Mustang");
+  const [year, setYear] = useState("1964");
   const [color, setColor] = useState("red");
+
    return (
     <>
-      <h1>My favorite color is {color}!</h1>
-      <button
-        type="button"
-        onClick={() => setColor("blue")} 
-      >Blue</button>
-      <button
-        type="button"
-        onClick={() => setColor("red")}
-      >Red</button>
-      <button
-        type="button"
-        onClick={() => setColor("pink")}
-      >Pink</button>
-      <button
-        type="button"
-        onClick={() => setColor("green")}
-      >Green</button>
+      <h1>My {brand}</h1>
+      <p>
+        It is a {color} {model} from {year}.
+      </p>
     </>
    )
 };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<FavoriteColor />);
+root.render(<Car />);
