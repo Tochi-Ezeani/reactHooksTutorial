5d9cb330aa3258ac924bc30c0ffc8873643b44ab
Barry <ezeanibarry1@gmail.com>
2024-03-04 12:47:43 +0100

    Updating Objects and Arrays in State

--- react-hooks-tutorial/src/index.js
+++ react-hooks-tutorial/src/index.js
@@ -1,23 +1,33 @@
-import React, { useState } from 'react';
-import ReactDOM from 'react-dom/client';
+import React, { useState } from "react";
+import ReactDOM from "react-dom/client";
 
 const Car = () => {
   const [car, setCar] = useState({
     brand: "Ford",
     model: "Mustang",
     year: "1964",
-    color: "red"
+    color: "red",
   });
 
-   return (
+  const updateColor = () => {
+    setCar((car) => {
+      return { ...car, color: "blue" };
+    });
+  };
+
+  return (
     <>
       <h1>My {car.brand}</h1>
       <p>
         It is a {car.color} {car.model} from {car.year}.
       </p>
+      <button
+        type="button"
+        onClick={updateColor}
+      >Blue</button>
     </>
-   )
+  );
 };
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Car />);
