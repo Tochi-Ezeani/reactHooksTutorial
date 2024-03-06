e618aa8ccca1f5062f15b78b4b48a55a3785c9d8
Barry <ezeanibarry1@gmail.com>
2024-03-06 13:02:53 +0100

    React useEffect hook

--- react-hooks-tutorial/src/index.js
+++ react-hooks-tutorial/src/index.js
@@ -1,23 +1,29 @@
 import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 
-const Timer = () => {
+const Counter = () => {
   const [count, setCount] = useState(0);
+  const [calculation, setCalculation] = useState(0);
 
   useEffect(
     () => {
-      setTimeout(
-        () => {
-          setCount((count) => count + 1);
-        }, 
-        1000
+      setCalculation(
+        () => count * 2
       )
     },
-    []
+    [count]
   );
 
-  return <h1>I've rendered {count} times!</h1>
+  return (
+    <>
+      <p>Count: {count}</p>
+      <button 
+        onClick={() => setCount((count) => count + 1)}
+      >+</button>
+      <p>Calculation: {calculation}</p>
+    </>
+  )  
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Timer />);
+root.render(<Counter />);
