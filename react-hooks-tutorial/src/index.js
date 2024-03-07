27af8c96677cc5b7ad5b460b3f6bee3f09195beb
Barry <ezeanibarry1@gmail.com>
2024-03-07 19:11:55 +0100

    useContext

--- react-hooks-tutorial/src/index.js
+++ react-hooks-tutorial/src/index.js
@@ -1,45 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, createContext, useContext } from "react";
 import ReactDOM from "react-dom/client";
+
+const UserContext = createContext();
 
 const Component1 = () => {
   const [user, setUser] = useState("Jesse Hall");
 
   return (
     <>
-      <h1>Hello {user}!</h1>
-      <Component2 user={user} />
+      <UserContext.Provider value={user}>
+        <h1>Hello {user}!</h1>
+        <Component2 />
+      </UserContext.Provider>
     </>
   ) 
 };
 
-const Component2 = ({user}) => {
+const Component2 = () => {
   return (
     <>
       <h1>Component 2</h1>
-      <Component3 user={user} />
+      <Component3 />
     </>
   )
 };
 
-const Component3 = ({user}) => {
+const Component3 = () => {
   return (
     <>
       <h1>Component 3</h1>
-      <Component4 user={user} />
+      <Component4 />
     </>
   )
 };
 
-const Component4 = ({user}) => {
+const Component4 = () => {
   return (
     <>
       <h1>Component 4</h1>
-      <Component5 user={user} />
+      <Component5 />
     </>
   )
 };
 
-const Component5 = ({user}) => {
+const Component5 = () => {
+  const user = useContext(UserContext);
+  
   return (
     <>
       <h1>Component 5</h1>
